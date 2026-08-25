5013056076fba730066415cb2a53e5289c7578c5
199906-del <1176557536@qq.com>
2026-08-25 17:44:53 +0800

    feature: 构建优化

--- src/vite.env.d.ts
+++ src/vite.env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+// 声明虚拟模块
+declare module 'virtual:svg-icons-register' {
+  // 这是一个 side-effect 导入，不需要导出任何内容
+  const content: any
+  export default content
+}
+
+// 如果有其他虚拟模块，也可以在这里声明
+declare module 'virtual:*' {
+  const content: any
+  export default content
+}
